85390f8e0d09d8889d059a5f7987955a4d6b711c
Ziyad Al-Zanbagi <rayceon@gmail.com>
2023-05-04 16:59:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const likes = 100;
+const dislikes = 35;
+
+let upCount = likes
+let downCount = dislikes
+
+let likesBtn = document.getElementById("likeBtn");
+let dislikesBtn = document.getElementById("dislikeBtn")
+likesBtn.innerText = "👍" + likes;
+dislikesBtn.innerText =  "👎"+ dislikes;
+
+
+window.onload = function() {
+    if(document.cookie && document.cookie.indexOf("voted") >-1) {
+        disableButtons();
+    }
+}
+
+function doLike() {
+    upCount++;
+    likesBtn.innerText = "👍" + upCount;
+    document.cookie = "username=John Doe";
+    disableButtons();
+    setCookie();
+    console.log("you liked this");
+}
+
+function doDislike() {
+    downCount++;
+    dislikesBtn.innerText = "👎" + downCount;
+    document.cookie = "username=John Doe";
+    disableButtons();
+    setCookie();
+    console.log("you disliked this");
+}
+
+function disableButtons() {
+    dislikesBtn.disabled = true;
+    likesBtn.disabled = true;
+}
+
+function setCookie() {
+    document.cookie = "voted=true; SameSite=Strict; Max-Age=60";
+}
